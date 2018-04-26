60df48950b9d211d6045ceaf93f11658eb4bdb13
estilag <38683710+estilag@users.noreply.github.com>
2018-04-26 10:40:24 +0200

    nuevo fichero modificado

--- procesado.py
+++ procesado.py
@@ -1,1 +1,17 @@
-print("Hi")
+#7.- La idea es hacer dos módulos el primer módulo deberá crear una función que cree un listado de datos, los datos a guardar serán de una clase llamada Cliente
+import modelo
+def creaListado():
+    List=Cliente.
+#8.- La clase cliente estará en el módulo llamado modelo, y dispondrá de los datos: nombre, dirección, tlf, email y facturación anual
+
+#9.- El segundo módulo se llamará procesado y tendrá que tener una función que sea capaz de devolver el total de la facturación de la
+# empresa sumando todos los totales de facturación anual de los clientes
+
+
+def facturacionTotal():
+    suma=cliente.factanual
+
+
+#10.- Crea una función más en el módulo de procesado que deberán devolver la media  de la facturación por cliente
+
+#11.-  El programa principal deberá importar los dos módulos, deberá llamar a la función que devuelve el listado de clientes ya rellenados y deberá llamar a las funciones que devuelvan la facturación total y la media de facturación por cliente
