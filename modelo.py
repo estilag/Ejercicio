572d5184000e4586cf6837f24e5fba2c35db5e74
Mabel <mabe.m1991@gmail.com>
2018-04-26 11:50:50 +0200

    Creado fichero de pruebas y verificada clase Cliente

--- modelo.py
+++ modelo.py
@@ -37,18 +37,9 @@ class Cliente:
         self.factanual = factanual
 
     # Función que devuelve el Listado con todos los datos
-    def getListado(self):
-        listado=["Nombre:", self.nombre, " Dirección:", self.dir, " Teléfono:", self.tlf," Email:", self.email, "Facturación Anual:", self.factanual]
+    def __str__(self):
+        listado = "Cliente[nombre:"+ self.nombre+ ", dir:"+ self.dir+ ", tlf:"+ str(self.tlf)+", email:"+ self.email+ ", factanual:"+ str(self.factanual)+"]"
         return listado
 
 # Creo cuatro clientes
 
-ana = Cliente("Ana","Florida 1",123445,"ana@a.com",10000)
-jose = Cliente("Jose","Florida 2",586879,"jose@a.com",20000)
-maria = Cliente("María","Florida 3",1258965,"maria@a.com",9000)
-juan = Cliente("Juan","Florida 4",946345,"juan@a.com",11000)
-
-print(ana.getListado())
-print(jose.getListado())
-print(maria.getListado())
-print(juan.getListado())
---
+++ pruebaModelo.py
@@ -0,0 +1,11 @@
+from  modelo import Cliente
+
+ana = Cliente("Ana","Florida 1",123445,"ana@a.com",10000)
+jose = Cliente("Jose","Florida 2",586879,"jose@a.com",20000)
+maria = Cliente("María","Florida 3",1258965,"maria@a.com",9000)
+juan = Cliente("Juan","Florida 4",946345,"juan@a.com",11000)
+
+print(ana)
+print(jose)
+print(maria)
+print(juan)
